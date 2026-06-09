de31d787ff160fcfdddb04fc69b9d8db198bd74c
ntfran <francisco.sandoval7700@gmail.com>
2026-06-09 10:36:17 -0300

    Agreagr ejemplos de variables y tipos de datos

---
+++ js/01-variables.js
@@ -0,0 +1,28 @@
+//Variables
+/* 
+Comentario de
+multiples
+lineas
+*/
+
+/* Crear una variables: var, let, const */
+
+const numero1 = 5;
+
+let nombre = "julian";
+console.log("variable nombre:", nombre);
+/* Modificar el nombre */
+
+nombre = "Estaban";
+
+/* Mostrar un mensaje en consola */
+
+console.log("variable nombre:", nombre);
+
+/* Tipos de datos: datos primitivos, valores por referencia */
+
+const numeroEntero = 10;
+const numeroDecimal = 10.5;
+
+console.log(typeof(numeroEntero), 'Numero Entero' + numeroEntero);
+console.log(typeof(numeroDecimal), 'Numero Decimal'+ numeroDecimal);
