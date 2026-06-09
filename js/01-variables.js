42a9dee12924f680a59305b2c21650ed51b45f5f
ntfran <francisco.sandoval7700@gmail.com>
2026-06-09 10:51:26 -0300

    Resolver ejercicio de sumar numeros

--- js/01-variables.js
+++ js/01-variables.js
@@ -26,3 +26,16 @@ const numeroDecimal = 10.5;
 
 console.log(typeof(numeroEntero), 'Numero Entero' + numeroEntero);
 console.log(typeof(numeroDecimal), 'Numero Decimal'+ numeroDecimal);
+/* console.log('Hola mundo' + numeroDecimal) */
+const saludo = 'hola mundo'
+console.log(typeof(saludo),saludo)
+
+let encendido = true
+console.log(typeof(encendido), encendido)
+
+let notafinal
+console.log(typeof(notafinal), notafinal)
+
+let carrito = null
+console.log(typeof(carrito), carrito)
+
---
+++ js/ejercicios/01-sumatoriaDeNumeros.js
@@ -0,0 +1,10 @@
+/* Realizar una suma de dos numeros y mostrar el resultado */
+let numero1, numero2, resultado;
+
+numero1 = parseInt(prompt('Ingresa el primer numero'))
+console.log(numero1)
+numero2 = parseInt(prompt('Ingresa el segundo numero'))
+console.log(numero2)
+resultado = numero1 + numero2
+
+console.log('El resultado es: ', resultado)
