07d1d0aed4926cc195eaf85bd42cf69474334034
ntfran <francisco.sandoval7700@gmail.com>
2026-06-10 01:55:47 -0300

    Resolver ejercicio de sumar numeros segunda version

---
+++ js/ejercicios/01-sumatoriaDeNumeros-version2.js
@@ -0,0 +1,10 @@
+/* Realizar una suma de dos numeros y mostrar el resultado */
+const numero1 = parseInt(prompt('Ingresa el primer numero'))
+console.log(numero1)
+
+const numero2 = parseInt(prompt('Ingresa el segundo numero'))
+console.log(numero2)
+
+const resultado = numero1 + numero2
+
+console.log('El resultado es: ', resultado)
