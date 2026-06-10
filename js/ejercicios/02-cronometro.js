37ee1c01a61be3c2322bc4386cba2b1b9fe8de09
ntfran <francisco.sandoval7700@gmail.com>
2026-06-10 02:06:48 -0300

    Resolver ejercicio de convertir horas a minutos

---
+++ js/ejercicios/02-cronometro.js
@@ -0,0 +1,5 @@
+const horas = parseInt(
+  prompt("Ingresar cantidad de horas a convertir en minutos"),
+);
+let minutos = horas * 60
+console.log(horas +' hora/s equivalen a '+ minutos +' minutos')
